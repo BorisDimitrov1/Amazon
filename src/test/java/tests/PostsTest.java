a0747936c1ba850348ef0a47ca86cc6132e22aa2
boris <b.dimitrov1@abv.bg>
2024-11-26 18:28:15 +0200

    Task 4 completed

--- src/test/java/tests/PostsTest.java
+++ src/test/java/tests/PostsTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.Test;
 import rest.config.ApiConfig;
 import rest.model.Post;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PostsTest {
 
     private static final String GET_POSTS_ENDPOINT = "/posts";
@@ -49,5 +52,32 @@ public class PostsTest {
 
         Assert.assertEquals(actualPostCount, expectedPostCount, "\r\nExpected Posts " + expectedPostCount + "\r\nActual Posts " + actualPostCount);
     }
+
+    @Test
+    public void Task4(){
+
+        Response response = ApiConfig.getRequestSpec()
+                .when()
+                .get(GET_POSTS_ENDPOINT)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.getStatusCode(), 200, "Expected response 200. Actual " + response.getStatusCode());
+
+        //Parse the response to Post objects
+        Post[] posts = response.getBody().as(Post[].class);
+
+        Set<Integer> ids = new HashSet<>();
+
+        //Validate each post has different ID
+        for(Post post : posts){
+            //If add returns false it means it already has the id in the collection
+            if(!ids.add(post.getId())){
+                throw new AssertionError("Duplicate Post ID " + post.getId());
+            }
+        }
+    }
+
 
 }
